d065f9a1fa90adc00e7074c809f3ee66bab1214c
K Dheeraj  Kumar <56029049+DheerajKumar97@users.noreply.github.com>
2020-07-12 16:00:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from scipy.stats import pearsonr
 import matplotlib.pyplot as plt 
 import matplotlib
 import seaborn as sns
+from PIL import Image
+image = Image.open('cover.jpg')
 matplotlib.use("Agg")
 
 def show_dtypes(x):
@@ -20,6 +22,9 @@ def show_columns(x):
 def Show_Missing(x):
     return x.isna().sum()
 def Show_Missing1(x):
+    return x.isna().sum()
+
+def Show_Missing2(x):
     return x.isna().sum()
 
 def show_hist(x):
@@ -56,6 +61,10 @@ def impute(x):
     df=x.dropna()
     return df
 
+def imputee(x):
+    df=x.dropna()
+    return df
+
 def Show_pearsonr(x,y):
     result = pearsonr(x,y)
     return result
@@ -73,6 +82,17 @@ def plotly(a,x,y):
                                             color='DarkSlateGrey')),
                       selector=dict(mode='markers'))
     fig.show()
+
+def show_displot(x):
+        plt.figure(1)
+        plt.subplot(121)
+        sns.distplot(x)
+
+
+        plt.subplot(122)
+        x.plot.box(figsize=(16,5))
+
+        plt.show()
 
 def Show_DisPlot(x):
     plt.style.use('fivethirtyeight')
@@ -106,27 +126,91 @@ def Show_PairPlot(x):
 def Show_HeatMap(x):
     f,ax = plt.subplots(figsize=(15, 15))
     return sns.heatmap(x.corr(),annot=True,ax=ax);
-    
-    
+
+def label(x):
+    from sklearn.preprocessing import LabelEncoder
+    le = LabelEncoder()
+    x=le.fit_transform(x)
+    return x
+
+def label1(x):
+    from sklearn.preprocessing import LabelEncoder
+    le = LabelEncoder()
+    x=le.fit_transform(x)
+    return x
+   
+def concat(x,y,z,axis):
+    return pd.concat([x,y,z],axis)
+
+def dummy(x):
+    return pd.get_dummies(x)
+
+import statsmodels.api as sm 
+def qqplot(x):
+    sm.qqplot(x, line ='45')
+
+from scipy.stats import anderson
+def Anderson_test(a):
+    return anderson(a)
+
+def PCA(x,y):
+    from sklearn.decomposition import PCA
+    pca =PCA(n_components=y)
+    principlecomponents = pca.fit_transform(x)
+    principledf = pd.DataFrame(data = principlecomponents)
+    return principledf
+
+def outlier(x):
+    high=0
+    q1 = x.quantile(.25)
+    q3 = x.quantile(.75)
+    iqr = q3-q1
+    low = q1-1.5*iqr
+    high += q3+1.5*iqr
+    outlier = (x.loc[(x < low) | (x > high)])
+    return(outlier)
+
+from scipy.stats import chi2_contingency,chi2
+
+def check_cat_relation(x,y,confidence_interval):
+    cross_table = pd.crosstab(x,y,margins=True)
+    stat,p,dof,expected = chi2_contingency(cross_table)
+    print("Chi_Square Value = {0}".format(stat))
+    print("P-Value = {0}".format(p))
+    alpha = 1 - confidence_interval
+    return p,alpha
+    if p > alpha:
+        print(">> Accepting Null Hypothesis <<")
+        print("There Is No Relationship Between Two Variables")
+    else:
+        print(">> Rejecting Null Hypothesis <<")
+        print("There Is A Significance Relationship Between Two Variables")
+
+from wordcloud import WordCloud
+def wordcloud(x):
+    wordcloud = WordCloud(width = 1000, height = 500).generate(" ".join(x))
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    return wordcloud
+
+st.image(image, use_column_width=True)   
 def main():
 	st.title("Machine Learning Application for Automated EDA")
-	html_temp = """
-	<div style="background-color:tomato;padding:10px">
-	<h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
-	</div>
-	"""
-	"""By DHEERAJ KUMAR"""
+	
+	st.info("This Web Application is created and maintained by *_DHEERAJ_ _KUMAR_ _K_*")
 	"""https://github.com/DheerajKumar97""" 
-	activities = ["EDA"]	
+	activities = ["General EDA","EDA For Linear Models","Feature Engineering","Model Building [IN Processs]"]	
 	choice = st.sidebar.selectbox("Select Activities",activities)
 
-	if choice == 'EDA':
+
+	if choice == 'General EDA':
 		st.subheader("Exploratory Data Analysis")
 
 		data = st.file_uploader("Upload a Dataset", type=["csv", "txt"])
 		if data is not None:
 			df = pd.read_csv(data)
 			st.dataframe(df.head())
+			st.success("Data Frame Loaded successfully")
 			
 
 			if st.checkbox("Show dtypes"):
@@ -250,10 +334,155 @@ def main():
 
 			if st.checkbox("Show PairPlot"):
 				st.write(Show_PairPlot(df))
-				st.pyplot()              
-                
+				st.pyplot()
+
+			if st.button("Generate Word Cloud"):
+				st.write(wordcloud(df))
+				st.pyplot()
+
+	elif choice == 'EDA For Linear Models':
+		st.subheader("EDA For Linear Models")
+		data = st.file_uploader("Upload a Dataset", type=["csv", "txt", "xlsx", "tsv"])
+		if data is not None:
+			df = pd.read_csv(data)
+			st.dataframe(df.head())
+			st.success("Data Frame Loaded successfully")
+
+
+			all_columns_names = show_columns(df)         
+			selected_columns_names = st.selectbox("Select Columns qqplot ",all_columns_names)
+			if st.checkbox("Show qqplot for variable"):
+				st.write(qqplot(df[selected_columns_names]))
+				st.pyplot()
+
+			all_columns_names = show_columns(df)         
+			selected_columns_names = st.selectbox("Select Columns outlier ",all_columns_names)
+			if st.checkbox("Show outliers in variable"):
+				st.write(outlier(df[selected_columns_names]))
+
+			# all_columns_names = show_columns(df)         
+			# selected_columns_names = st.selectbox("Select target ",all_columns_names)
+			# if st.checkbox("Anderson Normality Test"):
+			# 	st.write(Anderson_test(df[selected_columns_names]))	
+
+			if st.checkbox("Show Distplot Selected Columns"):
+				selected_columns_names = st.selectbox("Select Columns for Distplot ",all_columns_names)
+				st.dataframe(show_displot(df[selected_columns_names]))
+				st.pyplot()
+
+			con1 = show_columns(df)
+			con2 = show_columns(df)            
+			conn1 = st.selectbox("Select 1st Columns for chi square test",con1)
+			conn2 = st.selectbox("Select 2st Columns for chi square test",con2)
+			if st.button("Generate chi square test"):
+				st.write(check_cat_relation(df[conn1],df[conn2],0.5))
+			
+
+
+	elif choice == 'Feature Engineering':
+		st.subheader("Feature Engineering")
+		data = st.file_uploader("Upload a Dataset", type=["csv", "txt", "xlsx"])
+		if data is not None:
+			df = pd.read_csv(data)
+			st.dataframe(df.head())
+			st.success("Data Frame Loaded successfully")
+
+			if st.checkbox("Show Selected Columns1"):
+				selected_columns = st.multiselect("Select Columns1",show_columns(df))
+				new_df = df[selected_columns]
+				st.dataframe(new_df)
+
+			if st.checkbox("Numerical Variables"):
+				num_df = Numerical_variables(df)
+				numer_df=pd.DataFrame(num_df)                
+				st.dataframe(numer_df)
+
+			if st.checkbox("Categorical Variables"):
+				new_df = categorical_variables(df)
+				catego_df=pd.DataFrame(new_df)                
+				st.dataframe(catego_df)
+
+			if st.checkbox("DropNA Variable"):
+				df = imputee(df)
+				st.dataframe(df)
+
+			if st.checkbox("Columns Missing after DropNA"):
+				st.write(Show_Missing2(df))
+
+			all_columns_names7 = show_columns(df)          
+			selected_columns_names6 = st.selectbox("Select Columns FE ",all_columns_names7)
+			if st.button("Label Encode"):
+				label_df1=pd.DataFrame(label(df[selected_columns_names6]))
+				st.dataframe(label_df1)
+			
+
+			all_columns_names7 = show_columns(df)           
+			selected_columns_names5 = st.selectbox("Select Columns FE2 ",all_columns_names7)
+			if st.button("Label Encode Var 2"):
+				label_df2=pd.DataFrame(label(df[selected_columns_names5]))
+				st.dataframe(label_df2)
+
+			all_columns_names8 = show_columns(df)           
+			selected_columns_names8 = st.selectbox("Select Columns FE3 ",all_columns_names8)
+			if st.button("Label Encode Var 3"):
+				label_df3=pd.DataFrame(label(df[selected_columns_names8]))
+				st.dataframe(label_df3)
+
+			if st.checkbox("Generate LabelEncoded DataFrame"):
+				st.dataframe(concat(label_df1,label_df2,label_df3,1))
+
+			if st.checkbox("Dummay Variable"):
+				df = dummy(df)
+				st.dataframe(df)
+
+			if st.checkbox("Principle Component Analysis"):
+				df = PCA(df,10)
+				st.dataframe(df)
+
+	elif choice == 'Model Building':
+		st.subheader("Model Building [IN Processs]")
+		data = st.file_uploader("Upload a Dataset", type=["csv", "txt", "xlsx"])
+		if data is not None:
+			df = pd.read_csv(data)
+			st.dataframe(df.head())
+			st.success("Data Frame Loaded successfully")
+
+			if st.checkbox("Show Selected Columns for target"):
+				selected_columns_ = st.multiselect("Select Columns for seperation",show_columns(df))
+				sep_df = df[selected_columns_]
+				st.dataframe(sep_df)
+
+			if st.checkbox("Select Indpendent Data"):
+				x = sep_df.iloc[:,:-1]
+				st.dataframe(x)
+
+			if st.checkbox("Select Dependent Data"):
+				y = sep_df.iloc[:,-1]
+				st.dataframe(y)
+			if st.checkbox("Select X Train"):
+				from sklearn.model_selection import train_test_split
+				x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
+				st.dataframe(x_train)
+
+			if st.checkbox("Select x_test"):
+				from sklearn.model_selection import train_test_split
+				x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
+				st.dataframe(x_test)
+
+			if st.checkbox("Select y_train"):
+				from sklearn.model_selection import train_test_split
+				x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
+				st.dataframe(y_train)
+
+			if st.checkbox("Select y_test"):
+				from sklearn.model_selection import train_test_split
+				x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
+				st.dataframe(y_test)
+
+	st.markdown('Automation is **_really_ _cool_**.')
+	st.markdown('<style>h1{color: red;}</style>', unsafe_allow_html=True)
 	st.title("Credits and Inspiration")
-	"""https://github.com/vishalsiram"""    
+	"""https://pycaret.org/"""    
 
 
 if __name__ == '__main__':
